ebd8593eba6dc0511262d3ca46044a789f4fce0e
Hussain Parsaiyan <hus787@gmail.com>
2024-07-31 16:04:31 +0330

    some business logic and test structure added. checkpoint commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,103 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    http::StatusCode,
+    routing::{get, post},
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
+use tracing::{error, info};
+
+// A stateless application
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    // build our application with a route
+    let app = Router::new()
+        // `GET /` goes to `root`
+        // tbd
+        .route("/", get(string_ret).post(string_ret))
+        // `POST /schedule` goes to `create_schedule`
+        .route("/schedule", post(create_schedule));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn create_schedule(
+    // this argument parses the request body
+    // into a `ScheduleNeed` type
+    Json(payload): Json<ScheduleNeed>,
+) -> (StatusCode, Json<Vec<Shift>>) {
+    let shifts = schedule_worker_shifts(payload);
+    (StatusCode::OK, Json(shifts))
+}
+
+async fn string_ret() -> (StatusCode, String) {
+    error!("");
+    info!("{:?}", time::macros::datetime!(2020-01-01 00:00));
+    (StatusCode::OK, String::from("hello world\n"))
+}
+
+fn schedule_worker_shifts(payload: ScheduleNeed) -> Vec<Shift> {
+    std::time::Instant::now();
+    info!("{:?}", payload);
+    let mut shifts: Vec<Shift> = Vec::new();
+    let shift_packed_days: u32 = payload.work % (payload.workers.len() as u32);
+
+    vec![Shift { worker: 1 }, Shift { worker: 2 }]
+}
+
+// the input to `scheduler` handler
+#[derive(Deserialize, Debug)]
+struct ScheduleNeed {
+    work: u32,
+    workers: Vec<u32>,
+}
+
+#[derive(Serialize, Debug)]
+struct Shift {
+    worker: u32,
+    // start_time: time,
+    // end_time: time,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn all_shifts_are_eight_hours_long() {
+        // A worker has shifts
+        assert_eq!(1, 1);
+    }
+    #[test]
+    fn no_shift_if_no_work() {
+        assert_eq!(1, 1);
+    }
+    #[test]
+    fn no_shift_if_no_worker() {
+        assert_eq!(1, 1);
+    }
+    #[test]
+    fn fill_max_possible_workers_in_a_day() {
+        assert_eq!(1, 1);
+    }
+    #[test]
+    fn worker_cannot_have_more_than_one_shift_per_day() {
+        // A worker never has two shifts on the same day
+        // (assumption that no more than 1 per day)
+        assert_eq!(1, 1);
+    }
+    #[test]
+    fn shifts_are_in_24_hour_table_in_multiples_of_eight() {
+        // It is a 24 hour timetable 0-8, 8-16, 16-24
+        assert_eq!(1, 1);
+    }
+    #[test]
+    fn workers_are_provided_shifts_if_there_is_work() {
+        // A worker has shifts
+        assert_eq!(1, 1);
+    }
 }
